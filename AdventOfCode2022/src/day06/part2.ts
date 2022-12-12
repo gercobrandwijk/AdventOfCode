112b60b61420286a9bfbe0ae93839f7998bfdd82
Gerco Brandwijk <g.brandwijk@gobright.com>
2022-12-12 21:00:22 +0100

    Day06

---
+++ AdventOfCode2022/src/day06/part2.ts
@@ -0,0 +1,31 @@
+import * as _ from "lodash";
+import { end, read, start } from "../helpers";
+
+let { time, execution } = start(
+    [
+        { file: "test", answer: 29 },
+        { file: "input", answer: 2202 },
+    ],
+    false
+);
+
+let line = read("06", execution);
+
+let answer = 0;
+
+let startMessageLength = 14;
+
+for (let i = startMessageLength - 1; i < line.length; i++) {
+    let marker = {};
+
+    for (let j = 0; j < startMessageLength; j++) {
+        marker[line[i - j]] = 1;
+    }
+
+    if (Object.keys(marker).length === startMessageLength) {
+        answer = i + 1;
+        break;
+    }
+}
+
+end(time, answer, execution);
