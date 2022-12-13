656ef55085f09e231069971c1a49092a42f544b0
Gerco Brandwijk <g.brandwijk@gobright.com>
2022-12-13 22:17:37 +0100

    Day11

---
+++ AdventOfCode2022/src/day11/part2.ts
@@ -0,0 +1,76 @@
+import * as _ from "lodash";
+import { end, readAsLines, start } from "../helpers";
+
+let { time, execution } = start(
+    [
+        { file: "test", answer: 2713310158 },
+        { file: "input", answer: undefined },
+    ],
+    false
+);
+
+let lines = readAsLines("11", execution);
+
+class Monkey {
+    items: number[];
+    operation: string;
+    testDivisibleBy: number;
+    testTrueMonkey: number;
+    testFalseMonkey: number;
+
+    inspectations: number = 0;
+
+    constructor(lines: string[]) {
+        this.items = lines[1].split(':')[1].split(',').map(x => parseInt(x.trim()));
+
+        this.operation = lines[2].split('new = ')[1];
+
+        this.testDivisibleBy = parseInt(lines[3].split('by ')[1]);
+        this.testTrueMonkey = parseInt(lines[4].split('monkey ')[1]);
+        this.testFalseMonkey = parseInt(lines[5].split('monkey ')[1]);
+    }
+
+    operate(worryLevel: number): number {
+        let evalOperation = this.operation;
+
+        while (evalOperation.indexOf('old') >= 0) {
+            evalOperation = evalOperation.replace('old', worryLevel.toString());
+        }
+
+        return parseInt(eval(evalOperation));
+    }
+
+    test(worryLevel: number): boolean {
+        return worryLevel % this.testDivisibleBy === 0;
+    }
+}
+
+let monkeys = _.chunk(lines, 7).map(x => new Monkey(x));
+
+let moduleValue = monkeys.map(x => x.testDivisibleBy).reduce((prev, curr) => prev * curr, 1);
+
+let rounds = 10000;
+
+for (let i = 0; i < rounds; i++) {
+    for (let monkey of monkeys) {
+        for (let item of monkey.items) {
+            monkey.inspectations++;
+
+            let newWorryLevel: number = monkey.operate(item);
+
+            newWorryLevel = newWorryLevel % moduleValue;
+
+            if (monkey.test(newWorryLevel)) {
+                monkeys[monkey.testTrueMonkey].items.push(newWorryLevel);
+            } else {
+                monkeys[monkey.testFalseMonkey].items.push(newWorryLevel);
+            }
+        }
+
+        monkey.items = [];
+    }
+}
+
+let answer = _.sortBy(monkeys, x => x.inspectations).reverse().slice(0, 2).reduce((prev, curr) => prev * curr.inspectations, 1);
+
+end(time, answer, execution);
